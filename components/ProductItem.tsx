f2af6684596f0c0826afd99a50b7bb466f621a4b
vituhonorato <vituhonorato.89@gmail.com>
2023-02-09 15:28:41 -0300

    add ProductsItem.tsx and import in Index.tsx

---
+++ components/ProductItem.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+
+interface ProductElements {
+    products: {
+        name: string;
+        slug: string;
+        category: string;
+        image: string;
+        price: number;
+        brand: string;
+        rating: number;
+        numReviews: number;
+        countInStock: number;
+        description: string;
+    }
+
+
+}
+
+//paramater {product} will be turn the object product come fomr data.ts
+const ProductItem = ({product}:any) => {
+    return(
+        <div className="card">
+            <Link href={`/product/${product.slug}`}>
+                <img src={product.image}
+                alt={product.name}
+                className="rounded shadow"
+                />
+            </Link>
+
+            <div className="flex flex-col items-center justify-center p-5">
+             <Link href={`/product/${product.slug}`}>
+                <h2 className="text-lg">{product.name}</h2>
+             </Link>
+             <p className="mb-2">{product.brand}</p>
+             <p>R${product.price}</p>
+             <button className="primary-button">Add to Cart</button>
+            </div>
+        </div>
+    )
+}
+export default ProductItem
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import ProductItem from '@/components/ProductItem';
+import data from '@/utils/data';
 import React from 'react';
 
 export default function Home() {
   return (
-    <>
-     <h1 className="text-3xl font-bold underline">Home Page</h1>
+    <> 
+     <h1 className="text-3xl font-bold ">Home Page</h1>
+     <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+      {data.products.map((product)=> (
+        <ProductItem product={product} key={product.slug}></ProductItem>
+      ))}
+     </div>
+    
     </>
   )
 }
