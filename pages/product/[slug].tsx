b614212a31a9c8828ef37e4f73b6cbc832bc5c19
vituhonorato <vituhonorato.89@gmail.com>
2023-02-13 19:20:45 -0300

    set css in [slug].tsx

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -2,6 +2,8 @@ import Layout from "@/components/Layout";
 import React from "react";
 import { useRouter } from "next/router"
 import data from "@/utils/data";
+import Link from "next/link";
+import Image from "next/image";
 
 const ProductScreen = () => {
     const { query } = useRouter();
@@ -13,9 +15,41 @@ const ProductScreen = () => {
     }
     return(
         <> 
-          <Layout title={product?.name}>
-              <h1>{product.name}</h1>  
-          </Layout>
+          
+              <div className="py-2">
+                <Link href='/'>Back to products</Link>
+              </div>
+              <div className="grid md:grid-cols-4 md:gap-3">
+                <div className="md:col-span-2">
+                    <Image 
+                    src={product.image} 
+                    alt={product.name}
+                    width={640}
+                    height={640}
+                    
+                     ></Image>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <h1 className="text-lg">{product.name}</h1>
+                        </li>
+                        <li>Category: {product.category}</li>
+                        <li>Brand: {product.brand}</li>
+                        <li>{product.rating} of {product.numReviews} reviews</li>
+                        <li>Description: {product.description}</li>
+                    </ul>
+                </div>
+                <div>
+                    <div className="card p-5 ">
+                    <div className="mb-2 flex justify-between">
+                        <div>Price</div>
+                        <div>R${product.price}</div>
+                    </div>
+                    </div>
+                </div>
+              </div>
+          
         </>
     )
 
