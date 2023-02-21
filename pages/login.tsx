327ba2d21dbd74954082c7021824dccee7a91ded
vituhonorato <vituhonorato.89@gmail.com>
2023-02-21 20:03:15 -0300

    add formik and yup

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,14 +1,23 @@
 import Link from 'next/link'
 import React from 'react'
+import { useForm } from 'react-hook-form' 
 
 const  LoginScreen = () => {
+
+    const {handleSubmit, register, formState:{errors},} = useForm();
+    const submitHandler = ({email, password}:any) => {
+        return
+    }
+    const errorMsg:any = errors.email?.message
+    
   return (
     <>
-     <form className='mx-auto max-w-screen-md'>
+     <form className='mx-auto max-w-screen-md' onSubmit={handleSubmit(submitHandler)}>
         <h1 className='mb-4 text-xl'>Login</h1>
         <div className='mb-4'>
             <label htmlFor='email'>Email</label>
-            <input type="email" className='w-full' id='email' autoFocus></input>
+            <input type="email"
+            {...register('email',{required:'Please enter email',pattern:{value:/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+\.([a-z]+)?$/i}} )} className='w-full' id='email' autoFocus></input>{errors.email && (<div className='text-red-500'>{errorMsg}</div>)}
         </div>
         <div className='mb-4'>
             <label htmlFor='password'>Password</label>
