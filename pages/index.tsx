23844fe8ab2c9f1d5a1011a54a64a5019fd33f36
vituhonorato <vituhonorato.89@gmail.com>
2023-03-02 12:13:32 -0300

    in index.tsx add getServerSidePros() to send data from mongo db to component Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
 import ProductItem from '@/components/ProductItem';
+import Product from '@/models/Product';
 import data from '@/utils/data';
+import db from '@/utils/db';
 import React from 'react';
 
-const Home = () => {
+const Home = ({products}:{products:any}) => {
   return (
     <> 
      <h1 className="text-3xl font-bold ">Home Page</h1>
      <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
-      {data.products.map((product)=> (
+      {products.map((product:any)=> (
         <ProductItem product={product} key={product.slug}></ProductItem>
       ))}
      </div>
@@ -15,4 +17,17 @@ const Home = () => {
     </>
   )
 } 
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find().lean();
+  return {
+    props: {
+      products
+    }
+   }
+  }
+
+
+
 export default Home
