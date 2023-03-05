421a2621a4775ad68349039383279fb1f1aa1c67
vituhonorato <vituhonorato.89@gmail.com>
2023-03-05 15:57:25 -0300

    add page/order-history.tsx

---
+++ pages/order-history.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+
+const OrderHistory = () => {
+
+    useEffect(()=>{
+        
+    },[])
+  return (
+    <>
+    <h1>
+
+    </h1>
+    
+    </>
+  )
+}
+
+export default OrderHistory
