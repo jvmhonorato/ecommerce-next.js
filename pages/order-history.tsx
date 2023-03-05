fb24ecf82353b130c036bd10fdca8845efaef6ed
vituhonorato <vituhonorato.89@gmail.com>
2023-03-05 16:45:03 -0300

    finish buid order-history.tsx

--- pages/order-history.tsx
+++ pages/order-history.tsx
@@ -1,18 +1,82 @@
-import React, { useEffect } from 'react'
+import { getError } from '@/utils/error';
+import axios from 'axios';
+import Link from 'next/link';
+import React, { useEffect, useReducer } from 'react'
 
-const OrderHistory = () => {
+
+function reducer(state:any, action:any){
+    switch (action.type){
+        case 'FETCH_REQUEST':
+            return {...state, loading: true, error:''}
+        case 'FETCH_SUCCESS':
+            return {...state, loading: false, orders: action.payload, error:''}
+        case 'FETCH_FAL':
+            return {...state, loading: false, error: action.payload }    
+         default:
+            return state;   
+    }
+}
+
+const OrderHistoryScreen = () => {
+
+    const [{loading, error, orders},dispatch] = useReducer(reducer, {
+        loading:true,
+        orders: [],
+        error:'',
+    });
 
     useEffect(()=>{
-        
+        const fetchOrders = async () => {
+            try {
+                dispatch({type: 'FETCH_REQUEST'})
+                const { data } = await axios.get(`/api/orders/history`)
+                dispatch({type: 'FETCH_SUCCESS', payload: data})
+            } catch(err){
+                dispatch({type: 'FECTH_FAIL', payload: getError(err) });
+            }
+        }; 
+        fetchOrders();
     },[])
   return (
     <>
-    <h1>
-
-    </h1>
+    <h1 className='mb-4 text-lg'> Orders History Screen </h1>
+     {loading ? 
+     (<div>Loading...</div>
+     ) : error ? (
+     <div className='alert-error'>{error}</div>
+     ) : (
+     <div className='overflow-x-auto'>
+        <table className='min-w-full'>
+            <thead className='border-b'>
+                <tr>
+                    <th className='px-5 text-left'>ID</th>
+                    <th className='p-5 text-left'>DATE</th>
+                    <th className='p-5 text-left'>TOTAL</th>
+                    <th className='p-5 text-left'>PAID</th>
+                    <th className='p-5 text-left'>DELIVERED</th>
+                    <th className='p-5 text-left'>ACTION</th>
+                </tr>
+             </thead>
+             <tbody>
+                {orders.map((order:any)=> (
+                    <tr key={order._id} className='border-b'>
+                        <td className='p-5'>{order._id.substring(20, 24)}</td>
+                        <td className='p-5'>{order.createAt.substring(0, 10)}</td>
+                        <td className='p-5'>{order.totalPrice}</td>
+                        <td className='p-5'>{order.isPaid ? `${order.paidAt.substring(0, 10)}` : 'Not Paid'}</td>
+                        <td className='p-5'>{order.isDelivered ? `${order.deliveredAt.subtring(0, 10)}`: 'Note Delivered'}</td>
+                        <td className='p-5'><Link href={`/order/${order._id}`} passHref className='text-indigo-700'>Details</Link></td>
+                    </tr>
+                ))}
+             </tbody>
+        </table>
+     </div>
+     
+     )}
     
     </>
   )
 }
 
-export default OrderHistory
+OrderHistoryScreen.auth = true;
+export default OrderHistoryScreen
